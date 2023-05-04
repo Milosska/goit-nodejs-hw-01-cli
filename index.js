d7f45d83f87667d76e678b51688da5c97c9bbdbf
Milosska <110258611+Milosska@users.noreply.github.com>
2023-05-04 21:40:48 +0300

    commit for mentor

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const contacts = require("./contacts");
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contactsList = await contacts.listContacts();
+      console.log(contactsList);
+      break;
+
+    case "get":
+      const searchedContact = await contacts.getContactById(id);
+      console.log(searchedContact);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removedContact = await contacts.removeContact(id);
+      console.log(removedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+// invokeAction({
+//   action: "add",
+//   name: "Alice Green",
+//   email: "alise.green@mail.com",
+//   phone: "355-22-55",
+// });
