a542911f39e72fc6d12e09d25d3eb108863d2868
Milosska <110258611+Milosska@users.noreply.github.com>
2023-05-04 18:53:40 +0300

    initial commit + contactsPath

---
+++ contacts.js
@@ -0,0 +1,7 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+console.log(contactsPath);
+
+module.exports = {};
